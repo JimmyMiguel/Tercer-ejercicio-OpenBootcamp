da2f01dd2a3628071d4ad6e5076f6396dd84c847
JimmyMiguel <jimmymiguelgalarza@gmail.com>
2022-07-22 20:56:36 -0500

    tarea terminada

---
+++ src/DoWhile.java
@@ -0,0 +1,17 @@
+
+public class DoWhile {
+    public static void main(String[] args){
+
+        // Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+        int numeroWhile = 1;
+
+        do {
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+
+        } 
+            while (numeroWhile < 2 );
+            
+    }
+
+}
